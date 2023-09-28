82a3353a2b3741c6d165919cf7a4d7b0f29fa5a7
PorkyJames <kwanjko17@gmail.com>
2023-09-27 18:01:52 -0700

    Finished Models, moved onto Migrations

---
+++ backend/db/models/spot.js
@@ -0,0 +1,77 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spot.init({
+    ownerId: {
+      type: DataTypes.INTEGER,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lat: {
+      type: DataTypes.DECIMAL(10, 7),
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        min: -90,
+        max: 90,
+      }
+    },
+    lng: {
+      type: DataTypes.DECIMAL(10, 7),
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        min: -180,
+        max: 180,
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 7),
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
