54a8d625a6e83be72f0e00f2928051d1e2171e87
PorkyJames <kwanjko17@gmail.com>
2023-10-07 14:16:47 -0700

    Testing with options in the create-spot migration for build fail on render

--- backend/db/migrations/20230927223212-create-spot.js
+++ backend/db/migrations/20230927223212-create-spot.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { options } = require('../../routes/api/spots');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -64,7 +67,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Spots');
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -9,17 +9,17 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'ownerId',
         onDelete: 'CASCADE',
         hooks: true,
-      })
+      });
       User.hasMany(models.Review, {
         foreignKey: 'userId',
         onDelete: 'CASCADE',
         hooks: true,
-      })
+      });
       User.hasMany(models.Booking, {
         foreignKey: 'userId',
         onDelete: 'CASCADE',
         hooks: true,
-      })
+      });
     }
   };
 
