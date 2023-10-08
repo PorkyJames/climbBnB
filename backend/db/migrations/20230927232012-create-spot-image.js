64e1816fabe1955932d942ca601cf062b6e3fb71
PorkyJames <kwanjko17@gmail.com>
2023-10-08 14:27:09 -0700

    Fixed tableName

--- backend/db/migrations/20230927232012-create-spot-image.js
+++ backend/db/migrations/20230927232012-create-spot-image.js
@@ -42,7 +42,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    options.tableName("SpotImages")
+    options.tableName = "SpotImages"
     await queryInterface.dropTable(options);
   }
 };
