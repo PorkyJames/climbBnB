7106746b9a618023537c421bfeaf12cd97b10fc5
PorkyJames <kwanjko17@gmail.com>
2023-10-08 23:45:24 -0700

    Changed validateData to try and fix

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,38 +9,45 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 //use to help validate our data
 const validateData = [
-    check("address")
-    .exists({ checkFalsy: true })
-    .withMessage("Street address is required"),
-    check("city")
-    .exists({ checkFalsy: true })
-    .withMessage("City is required"),
-    check("state")
-    .exists({ checkFalsy: true })
-    .withMessage("State is required"),
-    check("country")
-    .exists({ checkFalsy: true })
-    .withMessage("Country is required"),
-    check("lat")
-    .exists({ checkFalsy: true })
-    .isFloat({min:-90,max:90})
-    .withMessage("Latitude is not valid"),
-    check("lng")
-    .exists({ checkFalsy: true })
-    .isFloat({min:-180,max:180})
-    .withMessage("Longitude is not valid"),
-    check("name")
-    .exists({ checkFalsy: true })
-    .isLength({ max: 50 })
-    .withMessage("Name must be less than 50 characters"),
-    check("description")
-    .exists({ checkFalsy: true })
-    .withMessage("Description is required"),
-    check("price")
-    .exists({ checkFalsy: true })
-    .withMessage("Price per day is required"),
+  check("address")
+  .exists({ checkFalsy: true })
+  .withMessage("Street address is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+  check("country")
+  .exists({ checkFalsy: true })
+  .withMessage("Country is required"),
+  check("lat")
+  .exists({ checkFalsy: true })
+  .isFloat({
+    min: -90,
+    max: 90,
+  })
+  .withMessage("Latitude is not valid"),
+  check("lng")
+  .exists({ checkFalsy: true })
+  .isFloat({
+    min: -180,
+    max: 180,
+  })
+  .withMessage("Longitude is not valid"),
+  check("name")
+  .exists({ checkFalsy: true })
+  .isLength({
+    max: 50,
+  })
+  .withMessage("Name must be less than 50 characters"),
+  check("description")
+  .exists({ checkFalsy: true })
+  .withMessage("Description is required"),
+  check("price")
+  .exists({ checkFalsy: true })
+  .isFloat({
+    min: 0,
+  })
+  .withMessage("Price per day is required and must be 0 or greater"),
 
-  handleValidationErrors
+handleValidationErrors,
 ];
 
 //used to help validate our booking
