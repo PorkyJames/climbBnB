b6f9506666cd08fc225ca0a8ccdd302bb48fc161
PorkyJames <kwanjko17@gmail.com>
2023-10-09 02:35:50 -0700

    Fixed additional logic

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -804,7 +804,7 @@ router.post("/:spotId/bookings", requireAuth, validateBooking, async (req, res, 
       const start = new Date(booking.startDate);
       const end = new Date(booking.endDate);
       //    start      startDate      end      endDate
-      if (start.getDate() === req.body.startDate.getDate() && end.getDate() === req.body.endDate.getDate()) {
+      if (new Date(startDate) - start >= 0 && end - new Date(startDate) >= 0) {
         const err = new Error(
           "Sorry, this spot is already booked for the specified dates"
         );
@@ -815,6 +815,13 @@ router.post("/:spotId/bookings", requireAuth, validateBooking, async (req, res, 
         return next(err);
       }
 
+      if (start.getDate() === req.body.startDate.getDate() && end.getDate() === req.body.endDate.getDate()) {
+        const err = new Error(
+          "Sorry, this spot is already booked for the specified dates"
+        );
+        err.status = 403;
+        return next(err);
+      }
 
       //   startDate    start    endDate    end
       if (new Date(endDate) - start >= 0 && end - new Date(endDate) >= 0) {
