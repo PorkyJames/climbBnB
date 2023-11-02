0748d95df7fb03ca3a8ad5d2478951c17f37fc9d
PorkyJames <kwanjko17@gmail.com>
2023-11-02 15:32:05 -0700

    Finished Phase 0

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 import configureStore from './store';
 
 //! Create a variable to access your store and expose it to window
@@ -12,6 +14,10 @@ import configureStore from './store';
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
