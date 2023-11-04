8e54cca80107e66ae563c7df20bd760620b822fb
PorkyJames <kwanjko17@gmail.com>
2023-11-04 14:39:21 -0700

    Finished Login Modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './App';
+import React from "react";
+
+import "./index.css";
+
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider, Modal } from "./context/Modal";
+import App from "./App";
+
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
-
-import { restoreCSRF, csrfFetch } from './store/csrf';
-
-import configureStore from './store';
-
-//! Create a variable to access your store and expose it to window
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
-
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -23,19 +22,25 @@ if (process.env.NODE_ENV !== 'production') {
   window.sessionActions = sessionActions;
 }
 
-function Root () {
+// Wrap the application with the Modal provider and render the Modal component
+// after the App component so that all the Modal content will be layered as
+// HTML elements on top of the all the other HTML elements:
+function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  )
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+  );
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
