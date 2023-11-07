bfd2772fd3d0042147fa5fc579d4292097fde581
PorkyJames <kwanjko17@gmail.com>
2023-11-07 11:40:36 -0800

    Added Review stars + the star icon for reviews in the TiledSpot landing page

--- frontend/src/components/TiledSpot/index.js
+++ frontend/src/components/TiledSpot/index.js
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
 
-const TiledSpot = ({ spot }) => {
 
+const TiledSpot = ({ spot }) => {
+    
     //! Get all of the spot information
     const { id, name, city, state, price, avgStarRating, previewImage, description } = spot;
 
+    //! Format our AvgStarRating to return to the 2nd decimal place
+    const formatAvgStarRating = (rating) => {
+        return rating.toFixed(2);
+    }
+
     //! If review exists / doesn't exist
-    let rating;
     const reviewExists = () => {
-        if (avgStarRating.length > 0) {
-            avgStarRating = rating
+        if (avgStarRating > 0) {
+            return avgStarRating
         } else {
-            rating = "New"
+            return "New"
         }
-        return rating;
     }
     
     return (
@@ -25,7 +29,12 @@ const TiledSpot = ({ spot }) => {
                     </div>
                     <div className="spot-city" title={spot.name}>{spot.name}</div>
                     <div className="spot-state" title={spot.name}>{spot.state}</div>
-                    <div className="spot-starRating" title={spot.name}>{reviewExists}</div>
+                    <div>
+                        <div className="spot-starRating" title={spot.name}>
+                            <i className="fas fa-star"></i>
+                            {formatAvgStarRating(reviewExists())}
+                        </div>
+                    </div>
                     <div className="spot-price" title={spot.name}>${spot.price}.00 / night</div>
                 </div>
             </Link>
